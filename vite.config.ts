d93a14210113a6274de1cd014b601e3f0e14329f
Ramon Barbosa <ramon.barbosa@w5i.com.br>
2026-07-25 14:21:31 -0300

    wip:

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,59 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
 
-export default defineConfig({
-  vite: {
-    server: {
-      port: 5173,
-      strictPort: true,
-    },
+export default defineConfig(({ command }) => ({
+  server: {
+    host: "::",
+    port: 5173,
+    strictPort: true,
   },
-  tanstackStart: {
-    server: { entry: "server" },
+  css: {
+    transformer: "lightningcss",
   },
-});
+  resolve: {
+    tsconfigPaths: true,
+    alias: { "@": `${process.cwd()}/src` },
+    dedupe: [
+      "react",
+      "react-dom",
+      "react/jsx-runtime",
+      "react/jsx-dev-runtime",
+      "@tanstack/react-query",
+      "@tanstack/query-core",
+    ],
+  },
+  optimizeDeps: {
+    include: [
+      "react",
+      "react-dom",
+      "react-dom/client",
+      "react/jsx-runtime",
+      "react/jsx-dev-runtime",
+    ],
+    ignoreOutdatedRequests: true,
+  },
+  plugins: [
+    tailwindcss(),
+    tanstackStart({
+      server: { entry: "server" },
+      importProtection: {
+        behavior: "error",
+        client: {
+          files: ["**/server/**"],
+          specifiers: ["server-only"],
+        },
+      },
+    }),
+    ...(command === "build"
+      ? [
+          nitro({
+            defaultPreset: "cloudflare-module",
+          }),
+        ]
+      : []),
+    viteReact(),
+  ],
+}));
